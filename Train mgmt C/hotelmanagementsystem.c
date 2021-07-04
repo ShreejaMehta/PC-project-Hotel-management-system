858c48e4578cdcb18dafb160a6cfc4542a41d28b
ShreejaMehta <shreejamehta2002@gmail.com>
2021-07-05 01:19:38 +0530

    main file added

---
+++ Train mgmt C/hotelmanagementsystem.c
@@ -0,0 +1,23 @@
+//header files
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+//user defined functions
+void admin();// admin funcrions like see personal info about the residents, their phone number, adhaar card, etc 
+void login();//stores login and password for admin
+void bookroom();// to book rooms for the customer(basically add)
+void cancelbooking();//to cancel the room booked (Basically delete)
+void printreceipt(); //self exp. 
+float cost(int , int);//to find total cost 
+
+int main()
+{
+
+return 0;
+}
+
+
+
+
